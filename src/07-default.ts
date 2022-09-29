eb873a3db7ad1e9f278f765f11e3efa631a8aea4
Gerardo Miranda <gerardodev25@gmail.com>
2022-09-28 23:37:35 -0400

    Parámetros por defecto

---
+++ src/07-default.ts
@@ -0,0 +1,12 @@
+export const createProduct = (id: string | number, stok: number = 12, isNew: boolean = false) => {
+  return { id, stok: stok ?? 12, isNew: isNew ?? false };
+};
+
+const p1 = createProduct(1, 1223, true);
+const p2 = createProduct(1, 0);
+const p3 = createProduct(31);
+
+console.log('/-----------------------------/');
+console.log(p1);
+console.log(p2);
+console.log(p3);
