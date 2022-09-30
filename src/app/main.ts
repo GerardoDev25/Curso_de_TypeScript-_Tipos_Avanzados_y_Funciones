6820ead6824a79bdb19956b54295f2ad93da2b31
Gerardo Miranda <gerardodev25@gmail.com>
2022-09-29 23:41:56 -0400

    Estructuras complejas

---
+++ src/app/main.ts
@@ -0,0 +1,10 @@
+import { addProduct } from './products/product.service';
+
+addProduct({
+  id: '1',
+  title: 'p1',
+  createdAt: new Date(),
+  stock: 90,
+  size: 'S',
+  category: { id: 1, name: 'c1' },
+});
