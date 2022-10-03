cc4e4f0cb6d501f3cb992951cc92d536b4e73a15
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-02 21:16:09 -0400

    Ejemplo de CRUD

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,27 @@
-import { addProduct } from './products/product.service';
+import faker from '@faker-js/faker';
+import { addProduct, products } from './products/product.service';
 
-addProduct({
-  id: '1',
-  title: 'p1',
-  createdAt: new Date(),
-  updateAt: new Date(),
-  stock: 90,
-  size: 'S',
-  category: {
-    id: '1',
-    name: 'c1',
-    createdAt: new Date(),
-    updateAt: new Date(),
-  },
-});
+for (let i = 0; i < 50; i++) {
+  addProduct({
+    id: faker.datatype.uuid(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.imageUrl(),
+    color: faker.commerce.color(),
+    price: Number.parseInt(faker.commerce.price()),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updateAt: faker.date.recent(),
+    stock: faker.datatype.number({ min: 10, max: 100 }),
+    category: {
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updateAt: faker.date.recent(),
+    },
+  });
+}
+
+console.log(products);
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,26 @@
 import { Product } from './product.model';
-export const products: Product[] = [];
+export let products: Product[] = [];
 
 export const addProduct = (data: Product) => {
   products.push(data);
+};
+
+export const updateProduct = (id: string, change: Product): Product | undefined => {
+  const index = products.find((prod, index) => prod.id === id && index);
+  if (typeof index == 'number') {
+    products[index] = change;
+    return products[index];
+  }
+  return undefined;
+};
+
+export const getProduct = (id: string): Product | undefined => {
+  return products.find((prod) => prod.id === id);
+};
+
+export const deleteProduct = (id: string): Product | undefined => {
+  const productfined = products.find((prod) => prod.id === id);
+  products = products.filter((product) => product.id !== id);
+
+  return productfined;
 };
